aaca372df3dadba18dcc2c94b5b4f86167b78576
HighCaffeine <ghwns9099@naver.com>
2025-08-27 23:19:54 +0900

    TargetFrameRate

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 public class GameManager : GenericSingleton<GameManager>
 {
+    [SerializeField] private int targetFrameRate = 60;  //Default - 60
+
     private new void Awake()
     {
         base.Awake();
+
+        Application.targetFrameRate = targetFrameRate;
     }
 }
