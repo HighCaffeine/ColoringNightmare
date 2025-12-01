59b8f7d2ed307b46999eab1c980e3d6c2a2cbf53
HighCaffeine <ghwns9099@naver.com>
2025-12-01 21:54:03 +0900

    Image Read/Write Enabled

--- Assets/Scripts/Game/Drawing/VisualSimilarity.cs
+++ Assets/Scripts/Game/Drawing/VisualSimilarity.cs
@@ -11,6 +11,7 @@ public class VisualSimilarity : MonoBehaviour
 
     public void UpdateFillAmount(float amount)
     {
+        Debug.Log($"{amount}");
         targetFillImage.fillAmount = amount;
     }
 }
